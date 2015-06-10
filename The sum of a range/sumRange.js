60a2d1602f36dc81c89664f0714c6a7e3f0943c6
Denys Bykanov <denys.bykanov@globallogic.com>
2015-06-10 18:15:02 +0300

    Added bonus assignment in "The sum of a range"

--- The sum of a range/sumRange.js
+++ The sum of a range/sumRange.js
@@ -1,15 +1,30 @@
-function range(startVal, finishVal) {
+function range(startVal, finishVal, step) {
   var result = [];
+  var count = 1;
 
-  if (arguments.length !== 2 || startVal > finishVal) {
+  if (isNaN(step)) step = 1;
+
+  if (arguments.length < 2 || arguments.length > 3 || startVal > finishVal) {
     return ' Incorrect data';
   }
 
-  for (var i = startVal; i <= finishVal; i++) {
-    result.push(i);
+  if (step > 0) {
+    for (var i = startVal; i <= finishVal; i++) {
+      if (step == 1 || count % step !== 0) result.push(i);
+      count++;
+    }
+  } else {
+    for (var j = finishVal; j >= startVal; j--) {
+      if (step == 1 || step == -1 || count % step !== 0) result.push(j);
+      count++;
+    }
   }
+
   return result;
 }
+
+console.log(range(1, 10, -2));
+console.log(range(2, 5, -1));
 
 function sum(arrVal) {
   var result = 0;
@@ -21,3 +36,5 @@ function sum(arrVal) {
 }
 
 console.log(sum(range(10, 15)));
+console.log(sum(range(1, 10, 2)));
+console.log(sum(range(1, 10, -2)));

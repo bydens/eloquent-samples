88e35a571e77646c04f91558038defdd7175e366
Denys Bykanov <denys.bykanov@globallogic.com>
2015-07-28 10:39:43 +0300

    add HTTP

---
+++ 18 HTTP/lib/content.js
@@ -0,0 +1,21 @@
+
+var types = ["text/plain",
+             "text/html",
+             "application/json",
+             "application/rainbows+unicorns"];
+
+function requestAuthor(type) {
+  var req = new XMLHttpRequest();
+  req.open("GET", "http://eloquentjavascript.net/author", false);
+  req.setRequestHeader("accept", type);
+  req.send(null);
+  return req.responseText;
+}
+
+types.forEach(function(type) {
+  try {
+    console.log(type + ":\n", requestAuthor(type), "\n");
+  } catch (e) {
+    console.log("Raised error: " + e);
+  }
+});
